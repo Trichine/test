43a4e382fd53ca4fc7a3061a0896b1c1617d0d64
Aboubakeur Trichine <aboubakeur.trichine@le-campus-numerique.fr>
2023-05-25 14:57:19 +0200

    rajouter les fichiers css,footer,header

---
+++ footer.php
@@ -0,0 +1,43 @@
+<footer>
+        <div class="column">
+            <h2>A propos</h2>
+            <ul>
+                <li>
+                    <a href="#">Fonctionnement du site</a>
+                </li>
+                <li>
+                    <a href="#">Conditions générales de vente</a>
+                </li>
+                <li>
+                    <a href="#">Données et confidentialité</a>
+                </li>
+            </ul>
+        </div>
+        <div class="column">
+            <h2>Nos hébergements</h2>
+            <ul>
+                <li>
+                    <a href="#">Charte qualité</a>
+                </li>
+                <li>
+                    <a href="#">Soumettre votre hôtel</a>
+                </li>
+            </ul>
+        </div>
+        <div class="column">
+            <h2>Assistance</h2>
+            <ul>
+                <li>
+                    <a href="#">Centre d'aide</a>
+                </li>
+                <li>
+                    <a href="#">Nous contacter</a>
+                </li>
+                <li>
+                    <a href="https://github.com/training-dev-fr/P2-Booki" target="_blank">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAi68fw3hBkE6l-vGLWYB9aRoSV5DWJ0zKJtAzpjYTMD83DwP5WU4D1N7eHx1ucPcZle8&amp;usqp=CAU" class="informations__github"> Voir le projet sur github
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </footer>
---
+++ header.php
@@ -0,0 +1,31 @@
+<<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booki</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/tablette.css" media="screen and (max-width: 992px)">
+    <link rel="stylesheet" type="text/css" href="css/mobile.css" media="screen and (max-width: 768px)">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="img/logo/Booki@3x.png" />
+    <meta name="google-site-verification" content="g_p0XZ2oWk4-WIp1xi39VJNtdGsVVaMJRXLhtLliBAI" />
+</head>
+<body>
+<header>
+    
+    <div>
+       <img src="img/logo/Booki@3x.png">
+   </div>
+   <nav>
+       <ul>
+           <li><a href="#hebergement">Hébergements</a></li>
+           <li><a href="#activite">Activités</a></li>
+       </ul>
+   </nav>
+
+</header>     
+</body>
